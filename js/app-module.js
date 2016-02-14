57838f2ff5e810707a01360c9f5e60185fc5e48d
candaras <mark.candaras@gmail.com>
2016-02-13 20:20:53 -0500

    Initial commit

---
+++ js/app-module.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function () {
+  'use strict';
+
+  /* @ngdoc object
+   * @name vday
+   * @description
+   *
+   */
+  angular.module('vday', ['ui.router', 'home']);
+})();
+//# sourceMappingURL=app-module.js.map
